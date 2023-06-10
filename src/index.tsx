3dd10cfed9f68fb742588dd8d96cdd6d9622b37e
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-06-10 13:08:56 +0400

    3 files modified, 2 added

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,36 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+//import 'https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css';
 
 import App from './App';
+
+import HomePage from '/pages/HomePage';
+import ProductPage from './pages/ProductPage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
